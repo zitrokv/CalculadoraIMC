24b984dc8a47d2a78cac710ed967dd939fe08c81
zitrokv <victor.vialasc@gmail.com>
2024-06-05 14:04:44 +0200

    Definiendo calculadora imc

--- app/src/main/java/com/example/calculadoraimc/MainActivity.kt
+++ app/src/main/java/com/example/calculadoraimc/MainActivity.kt
@@ -17,26 +17,10 @@ class MainActivity : AppCompatActivity() {
     lateinit var welcomeTextView : TextView
     lateinit var clickButton: Button
     var vez = 1
-    var random = Random()//.nextInt(0xff, 0xffffff)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        welcomeTextView = findViewById(R.id.welcomeTextView)
-        clickButton = findViewById(R.id.clickButton)
-
-
-        welcomeTextView.text = "Bienvenidos a la primera aplicacion Android"
-
-        clickButton.setOnClickListener {
-            welcomeTextView.text = "He hecho click ${vez++} veces en el botón"
-            it.setBackgroundColor(Color.CYAN)
-            it.setBackgroundColor(Color.BLUE)
-            //val colorAleatorio: Int =  123 //random
-
-            //var intColor = (Random(colorAleatorio.rangeUntil().nextInt(2..4))
-            //it.setBackgroundColor(getColor(colorAleatorio))
-        }
 
 
 
